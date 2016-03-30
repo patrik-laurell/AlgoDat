c1282edc23d5e33390773c17b82877053091fda0
Patrik Laurell <patrik.laurell@gmail.com>
2016-03-30 11:03:33 +0200

    woop

---
+++ patrik/labb1/stablemarriage.java
@@ -0,0 +1,106 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+class StableMarriage {
+    static LinkedList<Man> men = new LinkedList<Man>();
+    static Woman[] women;
+    static Man[] manArray;
+
+    public static void main(String[] args) {
+        parseInput();
+        matchMake();
+    }
+
+    private static void parseInput() {
+        Scanner sc = new Scanner(System.in);
+        String line;
+        boolean readNames = true;
+        while(sc.hasNextLine()) {
+            line = sc.nextLine();
+            if(line.equals("")) {
+                readNames = false;
+                continue;
+            } else if(line.charAt(0) == '#') {
+                continue;
+            } else if(line.charAt(0) == 'n') {
+                women = new Woman[Integer.parseInt(line.substring(2))];
+                manArray = new Man[Integer.parseInt(line.substring(2))];
+            } else if(readNames) {
+                int id = Integer.parseInt(line.substring(0, line.indexOf(' ')));
+                String name = line.substring(line.indexOf(' ') + 1);
+                if(id % 2 == 0) {
+                    women[(id-1)/2] = new Woman(name);
+                } else {
+                    Man m = new Man((id-1)/2, name);
+                    men.add(m);
+                    manArray[(id-1)/2] = m;
+                }
+            } else {
+                int id = Integer.parseInt(line.substring(0, line.indexOf(':')));
+                String[] strprefs = line.substring(line.indexOf(':') + 2).split(" ");
+                int[] prefs = new int[strprefs.length];
+                if(id % 2 != 0) {
+                    for(int i=0; i<prefs.length; i++) {
+                        prefs[i] = (Integer.parseInt(strprefs[i])-1)/2;
+                    }
+                    manArray[(id-1)/2].preferences = prefs;
+                } else {
+                    for(int i=0; i<prefs.length; i++) {
+                        prefs[(Integer.parseInt(strprefs[i])-1)/2] = i;
+                    }
+                    women[(id-1)/2].preferences = prefs;
+                }
+            }
+        }
+    }
+
+    private static void matchMake() {
+        while(men.size() > 0) {
+           Man m = men.pop();
+           Woman w = women[m.preferences[m.currentWoman++]];
+           if(w.man == null) {
+               w.man = m;
+               m.woman = w;
+           } else if(w.preferences[w.man.id] > w.preferences[m.id]) {
+               Man tmp = w.man;
+               w.man = m;
+               m.woman = w;
+               men.add(tmp);
+           }else {
+               men.add(m);
+           }
+        }
+        for(int i=0; i<manArray.length; i++) {
+            System.out.println(manArray[i].name + " -- " + manArray[i].woman.name);
+        }
+    }
+
+    private static class Man {
+        int[] preferences;
+        int currentWoman;
+        int id;
+        Woman woman;
+        String name;
+
+        public Man(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+    }
+
+    private static class Woman {
+        int[] preferences;
+        Man man;
+        String name;
+
+        public Woman(String name) {
+            this.name = name;
+        }
+    }
+}
+
+
+
---
+++ patrik/labb1/test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for l in $(ls ../algdes-labs/matching/data/*in.txt)
+do
+    name=${l##*/}
+    echo "Running "$name
+    name=${name%-*}-out.txt
+    java StableMarriage < $l > $name
+    diff ${l%/*}/$name $name
+done
